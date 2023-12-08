3e5e4c82fec163340ce1e1ffb7a7ed1ab7a0f8fd
vitor-sp <110577332+vitor-sp@users.noreply.github.com>
2023-12-08 09:04:57 -0500

    Improve logs (#4)
    * wip
    
    * wip
    
    * wip

--- lib/src/wave.dart
+++ lib/src/wave.dart
@@ -275,6 +275,71 @@ class _WaveWidgetState extends State<WaveWidget> with TickerProviderStateMixin {
         oldWidget.repeat != widget.repeat ||
         oldWidget.curve != widget.curve ||
         oldWidget.child != widget.child) {
+      ///Print the values that have changed
+      if (oldWidget.config != widget.config) {
+        final oc = oldWidget.config as CustomConfig;
+        final c = widget.config as CustomConfig;
+        print('colors: ' + (oc.colors != c.colors).toString());
+        print('gradients: ' + (oc.gradients != c.gradients).toString());
+        print(
+          'gradientBegin: ' + (oc.gradientBegin != c.gradientBegin).toString(),
+        );
+        print('gradientEnd: ' + (oc.gradientEnd != c.gradientEnd).toString());
+        print('durations: ' + (oc.durations != c.durations).toString());
+        print(
+          'heightPercentages: ' +
+              (oc.heightPercentages != c.heightPercentages).toString(),
+        );
+        print('blur: ' + (oc.blur != c.blur).toString());
+        print(
+          'enabledStrokes: ' +
+              (oc.enabledStrokes != c.enabledStrokes).toString(),
+        );
+      }
+
+      if (oldWidget.size != widget.size)
+        print("size changed ${oldWidget.size} -> ${widget.size}");
+
+      if (oldWidget.waveAmplitude != widget.waveAmplitude)
+        print(
+            "waveAmplitude changed ${oldWidget.waveAmplitude} -> ${widget.waveAmplitude}");
+
+      if (oldWidget.wavePhase != widget.wavePhase)
+        print(
+            "wavePhase changed ${oldWidget.wavePhase} -> ${widget.wavePhase}");
+
+      if (oldWidget.waveFrequency != widget.waveFrequency)
+        print(
+            "waveFrequency changed ${oldWidget.waveFrequency} -> ${widget.waveFrequency}");
+
+      if (oldWidget.heightPercentage != widget.heightPercentage)
+        print(
+            "heightPercentage changed ${oldWidget.heightPercentage} -> ${widget.heightPercentage}");
+
+      if (oldWidget.duration != widget.duration)
+        print("duration changed ${oldWidget.duration} -> ${widget.duration}");
+
+      if (oldWidget.backgroundColor != widget.backgroundColor)
+        print(
+            "backgroundColor changed ${oldWidget.backgroundColor} -> ${widget.backgroundColor}");
+
+      if (oldWidget.backgroundImage != widget.backgroundImage)
+        print(
+            "backgroundImage changed ${oldWidget.backgroundImage} -> ${widget.backgroundImage}");
+
+      if (oldWidget.isLoop != widget.isLoop)
+        print("isLoop changed ${oldWidget.isLoop} -> ${widget.isLoop}");
+
+      if (oldWidget.repeat != widget.repeat)
+        print("repeat changed ${oldWidget.repeat} -> ${widget.repeat}");
+
+      if (oldWidget.curve != widget.curve)
+        print("curve changed ${oldWidget.curve} -> ${widget.curve}");
+
+      if (oldWidget.child != widget.child) {
+        print("child changed ${oldWidget.child} -> ${widget.child}");
+      }
+
       _disposeAnimations();
       _initAnimations();
     }
